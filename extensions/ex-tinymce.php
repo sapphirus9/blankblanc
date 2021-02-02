fa389e47d854a50931c5908bd2442496d6426f77
sapphirus9 <naoki@sapphirus.biz>
2021-02-02 14:11:42 +0900

    ビジュアルエディタのスタイルを追加／修正

--- extensions/ex-tinymce.php
+++ extensions/ex-tinymce.php
@@ -12,20 +12,89 @@ add_filter('mce_buttons', 'add_mce_buttons');
 
 if (!function_exists('customize_tiny_mce')) {
   function customize_tiny_mce($init) {
-    $init['block_formats'] = '段落=p;見出し2=h2;見出し3=h3;見出し4=h4;見出し5=h5;見出し6=h6;Div=div';
+    $init['block_formats'] = '段落=p;見出し1=h1;見出し2=h2;見出し3=h3;見出し4=h4;見出し5=h5;見出し6=h6;<div>=div;<pre>=pre';
     $style_formats = array(
+      array(
+        'title' => 'マーカーを引く',
+        'items' => array(
+          array(
+            'title'    => '赤',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-red',
+          ),
+          array(
+            'title'    => 'オレンジ',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-orange',
+          ),
+          array(
+            'title'    => '黄',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-yellow',
+          ),
+          array(
+            'title'    => '緑',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-green',
+          ),
+          array(
+            'title'    => '青',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-blue',
+          ),
+          array(
+            'title'    => '紫',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-purple',
+          ),
+          array(
+            'title'    => 'ピンク',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-pink',
+          ),
+          array(
+            'title'    => '水色',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-sky',
+          ),
+          array(
+            'title'    => '灰色',
+            'inline'   => 'em',
+            'classes'  => 'text-highlighter text-highlighter-gray',
+          ),
+        ),
+      ),
+      array(
+        'title' => 'ブロック',
+        'items' => array(
+          array(
+            'title'          => '<div>で囲む',
+            'block'          => 'div',
+            'classes'        => 'wp-block-group section-block',
+            'wrapper'        => true,
+            'merge_siblings' => false,
+          ),
+          array(
+            'title'          => 'フロートによる回り込み解除',
+            'selector'       => 'div',
+            'classes'        => 'clearfix',
+            'wrapper'        => true,
+            'merge_siblings' => false,
+          ),
+        ),
+      ),
       array(
         'title' => '行間',
         'items' => array(
           array(
-            'title'    => '行間を狭める(狭い Lv1)',
+            'title'    => '行間を狭める (狭い Lv1)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,li,dl,dt,dd,img,table',
-            'classes'  => 'line-height-narrow-lv1'
+            'classes'  => 'line-height-narrow-lv1',
           ),
           array(
-            'title'    => '行間を狭める(より狭い Lv2)',
+            'title'    => '行間を狭める (より狭い Lv2)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,li,dl,dt,dd,img,table',
-            'classes'  => 'line-height-narrow-lv2'
+            'classes'  => 'line-height-narrow-lv2',
           ),
         ),
       ),
@@ -35,47 +104,47 @@ if (!function_exists('customize_tiny_mce')) {
           array(
             'title'    => 'マージン消去',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,li,dl,dt,dd,img,table,hr',
-            'classes'  => 'clear-margin'
+            'classes'  => 'clear-margin',
           ),
           array(
-            'title'    => '上マージン追加(狭い Lv1)',
+            'title'    => '上マージン追加 (狭い Lv1)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-top-lv1'
+            'classes'  => 'margin-top-lv1',
           ),
           array(
-            'title'    => '上マージン追加(中位 Lv2)',
+            'title'    => '上マージン追加 (中位 Lv2)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-top-lv2'
+            'classes'  => 'margin-top-lv2',
           ),
           array(
-            'title'    => '上マージン追加(広い Lv3)',
+            'title'    => '上マージン追加 (広い Lv3)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-top-lv3'
+            'classes'  => 'margin-top-lv3',
           ),
           array(
-            'title'    => '下マージン追加(狭い Lv1)',
+            'title'    => '下マージン追加 (狭い Lv1)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-bottom-lv1'
+            'classes'  => 'margin-bottom-lv1',
           ),
           array(
-            'title'    => '下マージン追加(中位 Lv2)',
+            'title'    => '下マージン追加 (中位 Lv2)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-bottom-lv2'
+            'classes'  => 'margin-bottom-lv2',
           ),
           array(
-            'title'    => '下マージン追加(広い Lv3)',
+            'title'    => '下マージン追加 (広い Lv3)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'margin-bottom-lv3'
+            'classes'  => 'margin-bottom-lv3',
           ),
           array(
             'title'    => '左マージン追加',
             'selector' => 'div,span,em,strong,img',
-            'classes'  => 'margin-left'
+            'classes'  => 'margin-left',
           ),
           array(
             'title'    => '右マージン追加',
             'selector' => 'div,span,em,strong,img',
-            'classes'  => 'margin-right'
+            'classes'  => 'margin-right',
           ),
         ),
       ),
@@ -83,19 +152,19 @@ if (!function_exists('customize_tiny_mce')) {
         'title' => 'その他',
         'items' => array(
           array(
-            'title'    => '回り込み解除',
+            'title'    => '段落などの回り込みを解除',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,dl,img,table,hr',
-            'classes'  => 'clear'
+            'classes'  => 'clear',
           ),
           array(
-            'title'    => 'ブロック内センタリング(段落)',
+            'title'    => 'ブロック内センタリング (段落)',
             'selector' => 'div,p',
-            'classes'  => 'block-center'
+            'classes'  => 'block-center',
           ),
           array(
-            'title'    => 'オーバーフロー(hidden)',
+            'title'    => 'オーバーフローを非表示 (hidden)',
             'selector' => 'div,p,h2,h3,h4,h5,h6,ul,ol,li,dl,dt,dd,img,table',
-            'classes'  => 'overflow-hidden'
+            'classes'  => 'overflow-hidden',
           ),
         ),
       ),
