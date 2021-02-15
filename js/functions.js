5d90355bbddd771140907e2398dcca0c857eda41
sapphirus9 <naoki@sapphirus.biz>
2021-02-15 10:40:40 +0900

    スクロール時のtableのcssとjsの挙動を修正

--- js/functions.js
+++ js/functions.js
@@ -131,14 +131,14 @@
   $(function () {
     var spd = 5; // スクロールステップ(px)
     var ex = function (obj) {
-      var ofx = $('table', obj).outerWidth() - $(obj).outerWidth();
+      var ofx = Math.floor($('table', obj).outerWidth() - $(obj).outerWidth());
       var psx = $(obj).scrollLeft();
-      if (psx <= 0) {
+      if (psx <= 3) {
         $('.table-left-arrow', obj).fadeOut(200);
       } else {
         $('.table-left-arrow', obj).fadeIn(200);
       }
-      if (psx >= ofx) {
+      if (psx >= ofx - 3) {
         $('.table-right-arrow', obj).fadeOut(200);
       } else {
         $('.table-right-arrow', obj).fadeIn(200);
@@ -150,15 +150,17 @@
       .each(function () {
         var mvx, timer;
         var obj = this;
-        ex(this);
         $('.table-left-arrow, .table-right-arrow', this).on({
           mousedown: function () {
             var dir = $(this).hasClass('table-right-arrow');
             mvx = $(obj).scrollLeft();
             timer = setInterval(function () {
-              var ofx = $('table', obj).outerWidth() - $(obj).outerWidth();
+              var ofx = Math.ceil($('table', obj).outerWidth() - $(obj).outerWidth());
               if (mvx >= 0 && mvx <= ofx) {
                 mvx = dir ? mvx + spd : mvx - spd;
+                if (mvx < 0) {
+                  mvx = 0;
+                }
                 $(obj).scrollLeft(mvx);
               } else {
                 $(obj).scrollLeft();
@@ -179,6 +181,9 @@
       .on('scroll', function () {
         ex(this);
       });
+    $(window).on('load resize', function () {
+      ex($('.table-area'));
+    });
   });
 
   /**
