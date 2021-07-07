b3258743a1b55a3585ed4f2e82c8aef6ecdb6822
sapphirus9 <naoki@sapphirus.biz>
2021-07-07 11:59:38 +0900

    css書き出しをdart-sassに変更とcss最適化プラグインを利用

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+/**
+ * 設定
+ */
+const cfg = {
+  mode     : 'development', // development または production
+
+  rootDir  : '', // 対象のトップディレクトリ
+  srcDir   : '', // ソースファイルのディレクトリ
+  scssDir  : 'scss', // 対象の scss ディレクトリ
+  scssFiles: '**/*.scss', // 対象の scss ファイル
+
+  distDir  : '', // 出力対象のディレクトリ
+  cssDist  : '', // css の出力先ディレクトリ
+  mapDist  : 'map', // map の出力先ディレクトリ（css 内）
+}
+
+const path = require('path')
+
+/**
+ * ディレクトリ形成
+ */
+const _root = {
+  src : path.join(__dirname, cfg.rootDir, cfg.srcDir),
+  dist: path.join(__dirname, cfg.rootDir, cfg.distDir),
+}
+const _src = {
+  scss: path.resolve(_root.src, cfg.scssDir, cfg.scssFiles),
+}
+const _dist = {
+  css: path.resolve(_root.dist, cfg.cssDist),
+}
+const mapDir = path.resolve(cfg.distDir, cfg.mapDist)
+const _map = {
+  scss: path.relative(mapDir, path.resolve(cfg.srcDir, cfg.scssDir)),
+}
+
+/**
+ * モジュール
+ */
+const { src, dest, watch, parallel } = require('gulp')
+const cache        = require('gulp-cached')
+const notify       = require('gulp-notify')
+const plumber      = require('gulp-plumber')
+const postcss      = require('gulp-postcss')
+const progeny      = require('gulp-progeny')
+const sass         = require('gulp-sass')(require('sass'))
+const sourcemaps   = require('gulp-sourcemaps')
+const autoprefixer = require('autoprefixer')
+const mergerules   = require('postcss-merge-rules')
+const smqueries    = require('postcss-sort-media-queries')
+
+/**
+ * SCSS
+ */
+// mode: development
+const ScssDev = () =>
+  src(_src.scss)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(cache('scss'))
+    .pipe(progeny())
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({
+      outputStyle: 'expanded',
+      indentType: 'space',
+      indentWidth: 2,
+    }))
+    .pipe(postcss([
+      autoprefixer({ cascade: false }),
+      mergerules(),
+      smqueries()
+    ]))
+    .pipe(sourcemaps.write(cfg.mapDist, {
+      includeContent: false,
+      sourceRoot: _map.scss,
+    }))
+    .pipe(dest(_dist.css))
+
+// mode: production
+const ScssProd = () =>
+  src(_src.scss)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(cache('scss'))
+    .pipe(progeny())
+    .pipe(sass.sync({
+      outputStyle: 'compressed',
+    }))
+    .pipe(postcss([
+      autoprefixer({ cascade: false }),
+      mergerules(),
+      smqueries()
+    ]))
+    .pipe(dest(_dist.css))
+
+exports.build = parallel(cfg.mode == 'production' ? ScssProd : ScssDev)
