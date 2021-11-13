6ddf85f6cceb640d2251ea56a5438f3f8ff78899
sapphirus9 <naoki@sapphirus.biz>
2021-11-13 22:13:03 +0900

    テーマカスタマイザにロゴ画像の設定を追加

---
+++ admin/bb-theme-customizer.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Theme Name: BlankBlanc
+ * Author: Naoki Yamamoto
+ * Description: テーマカスタマイザ
+ */
+
+/**
+ * ロゴ画像
+ */
+function bb_customize_logo($wp_customize) {
+  global $bb_theme_config;
+  $config_values = 'blankblanc_config_values';
+  $wp_customize->add_setting(
+    "{$config_values}[logo_image]",
+    array(
+      'default'    => $bb_theme_config['logo_image'],
+      'transport'  => 'postMessage',
+      'type'       => 'option',
+      'capability' => 'edit_theme_options',
+    )
+  );
+
+  $wp_customize->add_control(
+    new WP_Customize_Media_Control(
+      $wp_customize,
+      'control_logo',
+      array(
+        'label'       => 'ロゴ画像',
+        'section'     => 'title_tagline',
+        'settings'    => "{$config_values}[logo_image]",
+        'priority'    => 60,
+        'description' => <<< EOD
+    <p>ロゴ画像を変更することができます。<br>
+    その他の設定は<a href="./themes.php?page=blankblanc_config_edit">テーマオプション</a>で変更してください。</p>
+EOD,
+        'type'        => 'image',
+      )
+    )
+  );
+
+  $wp_customize->selective_refresh->add_partial(
+    "{$config_values}[logo_image]",
+    array(
+      'selector' => '#global-header .logo a',
+      'container_inclusive' => false,
+      'render_callback' => '_render',
+    )
+  );
+  function _render($partial = null) {
+    global $bb_theme_config;
+    $src = '';
+    if ($partial) {
+      $src = get_theme_mod($partial->id);
+      preg_match('!^(.*?)\[(.*?)\]$!', $partial->id, $keys);
+      $bb_option = get_option($keys[1]);
+      $src = $bb_option[$keys[2]];
+    }
+    if (!$src) {
+      return '<span class="site-title">' . $bb_theme_config['logo_alt'] . '</span>';
+    }
+    return '<img src="' . $src . '" alt="">';
+  }
+}
+add_action('customize_register', 'bb_customize_logo');
--- functions/config.php
+++ functions/config.php
@@ -30,6 +30,9 @@ if (version_compare(phpversion(), '5.6.20', '<')) {
 /**
  * テーマオプションを設定
  */
+define('BB_THEME_DIR', __DIR__);
+define('BB_ADMIN_DIR', dirname(__DIR__));
+
 function bb_setup_theme_config() {
   global $bb_theme_config, $bb_theme_default;
   $bb_theme_config = $bb_theme_default = bb_config_default();
@@ -43,8 +46,8 @@ function bb_setup_theme_config() {
     update_option('blankblanc_config_values', wp_unslash($bb_theme_config));
   }
   if (is_admin()) {
-    require_once dirname(__DIR__) . '/admin/bb-theme-option.php';
-    require_once dirname(__DIR__) . '/admin/bb-theme-taxonomy-option.php';
+    require_once BB_ADMIN_DIR . '/admin/bb-theme-option.php';
+    require_once BB_ADMIN_DIR . '/admin/bb-theme-taxonomy-option.php';
   }
   define('VERSION_PARAM', $bb_theme_config['version_param']);
 }
@@ -54,10 +57,10 @@ add_action('after_setup_theme', 'bb_setup_theme_config');
 /**
  * ファンクションの読み込み
  */
-require_once __DIR__ . '/core.php';
-require_once __DIR__ . '/basis.php';
-require_once __DIR__ . '/extensions.php';
-
+require_once BB_THEME_DIR . '/core.php';
+require_once BB_THEME_DIR . '/basis.php';
+require_once BB_THEME_DIR . '/extensions.php';
+require_once BB_ADMIN_DIR . '/admin/bb-theme-customizer.php';
 
 /**
  * ウィジェットのブロックエディターを停止
