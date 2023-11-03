8cdd33df40c43f273be893d587b5821ce8ba4c07
sapphirus9 <naoki@sapphirus.biz>
2023-11-03 10:40:20 +0900

    gulpfileの更新

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -2,22 +2,22 @@
  * 設定
  */
 module.exports = {
-  proxy    : 'https://wp.localhost',
-  cert     : '/home/sapphirus/.ssl/local-domain.crt', // httpsの場合、証明書ファイル
-  key      : '/home/sapphirus/.ssl/local-domain.key', // httpsの場合、秘密鍵ファイル
-  host     : '192.168.1.32', // IPアドレス等
-  mode     : 'production', // developmentまたはproduction
+  proxy     : 'https://wp.localhost', // ローカル上のURL
+  cert      : '/home/sapphirus/.ssl/local-domain.crt', // httpsの場合、証明書ファイル
+  key       : '/home/sapphirus/.ssl/local-domain.key', // httpsの場合、秘密鍵ファイル
+  host      : '192.168.1.32', // ローカルPCのIPアドレス等
+  mode      : 'production', // developmentまたはproduction
 
-  rootDir  : '', // 対象のトップディレクトリ
-  srcDir   : 'src', // ソースファイルのディレクトリ
-  scssDir  : 'scss', // 対象のscssディレクトリ
-  scssFiles: '**/*.scss', // 対象のscssファイル
+  rootDir   : '', // 対象のトップディレクトリ
+  srcDir    : 'src', // ソースファイルのディレクトリ
+  scssDir   : 'scss', // 対象のscssディレクトリ
+  scssFiles : '**/*.scss', // 対象のscssファイル
   esmDir    : 'js', // 対象のesm(js)ディレクトリ
   esmFiles  : '**/*.js', // 対象のesm(js)ファイル
 
-  distDir  : 'assets', // 出力対象のディレクトリ
-  cssDist  : 'css', // cssの出力先ディレクトリ
-  cssMap   : 'css', // css mapの出力先ディレクトリ
-  jsDist   : 'js', // jsの出力先ディレクトリ
-  jsMap    : 'js', // js mapの出力先ディレクトリ
+  distDir   : 'assets', // 出力対象のディレクトリ
+  cssDist   : 'css', // cssの出力先ディレクトリ
+  cssMap    : '', // css mapの出力先ディレクトリ
+  jsDist    : 'js', // jsの出力先ディレクトリ
+  jsMap     : 'js', // js mapの出力先ディレクトリ
 }
